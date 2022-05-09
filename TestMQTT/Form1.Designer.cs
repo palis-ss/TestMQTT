683fb1d96647840159dedaabf49c0181b0cfade4
palis-ss <54706343+palis-ss@users.noreply.github.com>
2022-05-09 07:08:20 +0700

    Add project files.

---
+++ TestMQTT/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace TestMQTT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(247, 338);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(203, 60);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ TestMQTT/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Text.Json;
+
+
+namespace TestMQTT
+{
+    public class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string Summary { get; set; }
+    }
+
+
+    public partial class Form1 : Form
+    {        
+        private string mqttServer = "broker.hivemq.com";
+        private string clientID = "DTI_CANSAT_GROUND";
+        private MqttClient mqttClient;
+
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            MqttClient_PublishMessage();
+        }
+
+        void MqttClient_Initialize()
+        {
+            mqttClient = new MqttClient(mqttServer);            
+            mqttClient.MqttMsgPublishReceived += MqttClient_MqttMsgPublishReceived;
+            
+            mqttClient.Connect(clientID);
+        }
+
+        void MqttClient_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Message);
+            try
+            {
+                WeatherForecast weatherForecast =
+                    JsonSerializer.Deserialize<WeatherForecast>(message);
+
+                //Debug.WriteLine($"Received: {e.Topic}, {message}");
+                Debug.WriteLine(weatherForecast.Date);
+                Debug.WriteLine(weatherForecast.TemperatureCelsius);
+                Debug.WriteLine(weatherForecast.Summary);
+            }
+            catch (Exception ex)
+            { Debug.WriteLine(ex.Message); }
+        }
+
+        private void MqttClient_PublishMessage()
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+            string jsonString = JsonSerializer.Serialize(weatherForecast);
+
+            if (!mqttClient.IsConnected)
+                mqttClient.Connect(clientID);
+
+            if (mqttClient.IsConnected)
+                // publish a message on "/home/temperature" topic with QoS 2
+                mqttClient.Publish("mymqtt/testmessage", Encoding.UTF8.GetBytes(jsonString), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+            else
+                Debug.WriteLine("Can't connect to server");
+        }
+
+        private void MqttClient_SubscribeTopic()
+        {            
+            string[] topic = { "testtopic/1", "sensor/humidity" };
+
+            byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE };
+            mqttClient.Subscribe(topic, qosLevels);
+
+            Debug.WriteLine("Subsribed to topic");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MqttClient_Initialize();
+            MqttClient_SubscribeTopic();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            mqttClient.Disconnect();
+        }
+    }
+}
